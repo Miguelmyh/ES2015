d60d6cbe29faeb0c6fa629706c4b0f9e069a6fad
Miguel <miguelmyh14@gmail.com>
2023-04-09 15:42:53 -0400

    finish objectEnhancement exercises

---
+++ es2015/objectEnhancements/objectEnhancement.js
@@ -0,0 +1,41 @@
+function createInstructor(firstName, lastName) {
+    return {
+        firstName: firstName,
+        lastName: lastName
+    }
+}
+
+const createInstructor = (first, last) => {
+    return {
+        first,
+        last
+    }
+}
+
+///////
+const favoriteNum = 42;
+
+const instructor = {
+    firstName: 'colt',
+    [favoriteNum]: "that is my favorite"
+}
+
+const instructor2 = {
+  firstName: "Colt",
+  sayHi(){
+    return "Hi!";
+  },
+  sayBye(){
+    return this.firstName + " says bye!";
+  }
+}
+
+
+const createAnimal = (species, verb, noise) => {
+    return {
+        species, 
+        [verb](){
+            return noise;
+        }
+    }
+}
